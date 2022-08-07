abc97ebc8367bd0b6873dc73a8f57f6824803573
vetrivel <vetrivel.kce@gmail.com>
2022-08-07 12:46:57 +0530

    ucfive

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-namespace SnakeAndLader
+namespace SnakeAndLadder
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            WinningPosition.WinOrRestart();
+            WinningPoint.WinPoint();
         }
     }
 }
---
+++ WinningPoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class WinningPoint
+    {
+        public static void WinPoint()
+        {
+            const int winningPoint = 1;
+            Random random = new Random();
+            int dice = random.Next(7);
+            Console.WriteLine("Dice Number is : " + dice);
+            if (dice == winningPoint)
+            {
+                Console.WriteLine("Player reached the winning position");
+            }
+            else
+            {
+                Console.WriteLine("Player stays at same position");
+            }
+        }
+
+    }
+}
