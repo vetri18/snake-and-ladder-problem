aaa22a8aa368706a9295268103407bb15b20150b
vetrivel <vetrivel.kce@gmail.com>
2022-08-07 12:29:42 +0530

    welcome to snake and ladder program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace Snakeandladder
+{
+    class gameone
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Welcome To Snake And Ladder game ");
+         
+        }     
+     }
+}
