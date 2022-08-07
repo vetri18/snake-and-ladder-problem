3e7a0a4c5a7d1c310eb7948ed86cb8ab0b47a30e
vetrivel <vetrivel.kce@gmail.com>
2022-08-07 12:45:06 +0530

    ucfour

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
-namespace SnakeAndLadderGame
+namespace SnakeAndLader
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            DiceCheck.CheckOption();
+            WinningPosition.WinOrRestart();
         }
     }
 }
-
